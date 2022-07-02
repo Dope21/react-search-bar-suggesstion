1af84c44cc332dcbe341eef930f188b6cd582f06
Thanasak <bestzacap20@gmail.com>
2022-07-02 14:01:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './customer.css'
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [text, setText] = useState('')
+  const [search, setSearch] = useState([])
+
+  useEffect(() => {
+    const loadUsers = async() => {
+      const response = await axios.get('https://reqres.in/api/users')
+      setUsers(response.data.data)
+    }
+    loadUsers();
+  },[])
+
+  const onChangeHandler = (e) => {
+    const input = e.target.value
+    let matches = []
+    
+    if (input.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${input}`, 'gi')
+        return user.email.match(regex)
+      })
+    }
+    console.log('matches', matches)
+    setSearch(search => search = matches)
+    setText(text => text = input)
+  }
+
+  const onSearchHandler = (input) => {
+    setText(text => text = input)
+    setSearch([])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    <div className="container">
+      <input 
+        className='col-md-12 input'
+        style={{ marginTop: 10 }}
+        type="text" 
+        onChange={onChangeHandler}
+        value={text}
+        onBlur={ () => setTimeout(() => {
+          setSearch([])
+        }, 100)}
+      />
+      {search && search.map((user, i) => 
+        <div 
+          key={i} 
+          className="suggestion col-md-12 justify-content-md-center"
+          onClick={() => onSearchHandler(user.email)}
         >
-          Learn React
-        </a>
-      </header>
+          {user.email}
+        </div>
+      )}
     </div>
   );
 }
